e892263485c037b7cf828ef786875ab79dc4b4a2
Nico42 <nicolas.gamin@ynov.com>
2017-02-09 17:37:39 +0100

    test de connexion réussi

---
+++ Douche-Bot/TestConnIRC2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Douche_Bot;
+
+namespace TestConnIRC2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             botName = "Douche-Bot";
+             port = 6667;
+             server = "irc.chat.twitch.tv";
+            */
+            // infoConnect Bot = new  infoConnect("Douche-Bot", "irc.chat.twitch.tv", "oauth:ymm54b9x2zs48ofbx46x2ghxmt4dju");
+
+            // "Douche-Bot", "irc.chat.twitch.tv", "oauth:ymm54b9x2zs48ofbx46x2ghxmt4dju"
+
+            Console.WriteLine("Test Connexion twitch \n");
+            IRCbot ircBot = new IRCbot(
+                server: "irc.chat.twitch.tv",
+                port: 6667,
+                user: "altha_n16",
+                nick: "Douche-Bot",
+                channel: "#Casual",
+                pass : "oauth:ymm54b9x2zs48ofbx46x2ghxmt4dju"
+            );
+
+            ircBot.Start();
+            
+        }
+    }
+}
