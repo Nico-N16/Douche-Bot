b55e1d678f4bb864ce1bc601ddf69d94bbac2b9b
Nico42 <nicolas.gamin@ynov.com>
2017-02-09 12:39:09 +0100

    Blanc projet

---
+++ Douche-Bot/Douche-Bot/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Douche_Bot
+{
+    public class Class1
+    {
+    }
+}
